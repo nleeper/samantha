65c9e3802627518b94bd51e726677b6d1493556e
Nick Leeper <nick.leeper@gmail.com>
2017-11-21 22:26:52 -0600

    add ability to play an artist on a specified speaker

--- plugins/sonos.py
+++ plugins/sonos.py
@@ -5,7 +5,7 @@ import tornado.gen
 class Sonos(object):
     def __init__(self, manager):
         self._manager = manager 
-        self._intent_map = { 'play_artist': 'sonos.play', 'speaker_list': 'sonos.speakers' }
+        self._intent_map = { 'play_artist': 'sonos.play_artist', 'speaker_list': 'sonos.speakers' }
 
     def supported_intents(self):
         return list(self._intent_map.keys())
@@ -13,7 +13,9 @@ class Sonos(object):
     @tornado.gen.coroutine
     def handle(self, intent, entities):
         method = self._to_method(intent)
-        response = yield self._manager.pipeline.send_request_response(method, entities)
+        params = self._to_params(entities)
+
+        response = yield self._manager.pipeline.send_request_response(method, params)
         raise tornado.gen.Return(self._process_response(method, response))
 
     def _process_response(self, method, response):
@@ -23,6 +25,8 @@ class Sonos(object):
                 if method == 'sonos.speakers':
                     speakers = ', '.join([v['name'] for k, v in result['speakers'].iteritems()])
                     return 'I found these speakers: %s' % speakers
+                elif method == 'sonos.play_artist':
+                    return 'Your artist is playing. Enjoy!'
                 else:
                     return "I'm not sure what you wanted me to do"
             else:
@@ -32,3 +36,6 @@ class Sonos(object):
 
     def _to_method(self, intent):
         return self._intent_map[intent]
+
+    def _to_params(self, entities):
+        return {e['entity']: e['value'] for e in entities}
