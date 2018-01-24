39d5c6734bef05767403dbd4ea73a120ea5bc843
nleeper <nick.leeper@gmail.com>
2018-01-23 22:43:49 -0600

    use pytest for testing

--- setup.py
+++ setup.py
@@ -1,32 +1,29 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-def spacy_download_en():
-    import subprocess
-    args = ['python -m spacy download en']
-    subprocess.call(args, shell=True)
-
 class DownloadSpacy(install):
     def run(self):
         install.do_egg_install(self)
-        spacy_download_en()
-        install.run(self)
+        import spacy
+        spacy.cli.download('download', 'en')
 
 INSTALL_REQUIRES = [
-    'rasa-nlu==0.8.9',
-    'python-crfsuite==0.9.2',
-    'numpy==1.12.1',
-    'scipy==0.19.0',
-    'spacy==1.8.2',
+    'rasa-nlu==0.10.6',
+    'python-crfsuite==0.9.5',
+    'numpy==1.14',
+    'scipy==1.0.0',
+    'spacy==2.0.5',
     'tornado==4.5.1',
     'spotipy==2.4.4',
-    'scikit-learn==0.18.1',
+    'scikit-learn==0.19.1',
     'python-decouple==3.1',
-    'requests==2.16.0'
+    'requests==2.16.0',
+    'sklearn-crfsuite==0.3.6'
 ]
 
 SETUP_REQUIRES = [
-    'spacy==1.8.2'
+    'spacy==2.0.5',
+    'pytest-runner'
 ]
 
 TESTS_REQUIRE = [
